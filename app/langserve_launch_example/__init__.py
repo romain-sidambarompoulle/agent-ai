987d65d7db181a5d3c7daf594e043b821196f115
romain-sidambarompoulle <romain.sidambarompoulle@gmail.com>
2025-05-02 14:52:51 +0400

    fix: relative import in langserve_launch_example/__init__.py

--- app/langserve_launch_example/__init__.py
+++ app/langserve_launch_example/__init__.py
@@ -1,7 +1,7 @@
 """langserve_launch_example package."""
 from importlib import metadata
 
-from langserve_launch_example.chain import get_chain
+from .chain import get_chain          # import relatif = toujours valable où que soit le paquet
 
 try:
     __version__ = metadata.version(__package__)
